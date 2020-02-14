860566dae92df61be22cc695df7a6c374a3359ff
黄宇宁 <huangyuning@lingkehudong.com>
2020-02-15 00:59:22 +0800

    add: Header module

---
+++ components/Header/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './style.scss';
+import { Row, Col, Menu, Icon } from 'antd';
+
+const Header = () => (
+  <div className="header">
+    <Row type="flex" justify="space-between" align="bottom">
+      <Col xs={24} sm={24} md={10} lg={10} xl={10}>
+        <span className="logo">HYN</span>
+        <span className="headerText">慢慢琢磨一些技术。。。</span>
+      </Col>
+      <Col xs={0} sm={0} md={14} lg={8} xl={6}>
+        <Menu mode="horizontal" className="menuStyle">
+          <Menu.Item key="allArticles">
+            <Icon type="appstore" />
+            全部文章
+          </Menu.Item>
+
+          <Menu.Item key="tech">
+            <Icon type="bug" />
+            技术
+          </Menu.Item>
+
+          <Menu.Item key="learning">
+            <Icon type="book" />
+            学习
+          </Menu.Item>
+        </Menu>
+      </Col>
+    </Row>
+  </div>
+);
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Head from 'next/head'
 import {Button} from 'antd'
+import Header from '../components/Header'
 
 const Home = () => (
   <div>
     <Head>
-      <title>Home</title>
+      <title>HYN's Blog</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-
+    <Header />
     <div>
       <h1>测试</h1>
       <Button>按需引入antd</Button>
