4c9e9775ff552230f85de5dd4de345e69c850b21
黄宇宁 <huangyuning@lingkehudong.com>
2020-02-19 03:33:37 +0800

    fix bugs

--- pages/detail.js
+++ pages/detail.js
@@ -13,8 +13,7 @@ import './detail.scss';
 
 const Detail = () => {
   let markdown =
-    '#\n' +
-    '# p01:课程介绍和环境搭建\n' +
+    '## p01:课程介绍和环境搭建\n' +
     '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
     '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
     '**这是加粗的文字**\n\n' +
@@ -22,28 +21,28 @@ const Detail = () => {
     '***这是斜体加粗的文字***\n\n' +
     '~~这是加删除线的文字~~ \n\n' +
     '`console.log(111)` \n\n' +
-    '# p02:来个Hello World 初始Vue3.0\n' +
+    '## p02:来个Hello World 初始Vue3.0\n' +
     '> aaaaaaaaa\n' +
     '>> bbbbbbbbb\n' +
     '>>> cccccccccc\n' +
     '***\n\n\n' +
-    '# p03:Vue3.0基础知识讲解\n' +
+    '## p03:Vue3.0基础知识讲解\n' +
     '> aaaaaaaaa\n' +
     '>> bbbbbbbbb\n' +
     '>>> cccccccccc\n\n' +
-    '# p04:Vue3.0基础知识讲解\n' +
+    '## p04:Vue3.0基础知识讲解\n' +
     '> aaaaaaaaa\n' +
     '>> bbbbbbbbb\n' +
     '>>> cccccccccc\n\n' +
-    '# p05:Vue3.0基础知识讲解\n' +
+    '## p05:Vue3.0基础知识讲解\n' +
     '> aaaaaaaaa\n' +
     '>> bbbbbbbbb\n' +
     '>>> cccccccccc\n\n' +
-    '# p06:Vue3.0基础知识讲解\n' +
+    '## p06:Vue3.0基础知识讲解\n' +
     '> aaaaaaaaa\n' +
     '>> bbbbbbbbb\n' +
     '>>> cccccccccc\n\n' +
-    '# p07:Vue3.0基础知识讲解\n' +
+    '## p07:Vue3.0基础知识讲解\n' +
     '> aaaaaaaaa\n' +
     '>> bbbbbbbbb\n' +
     '>>> cccccccccc\n\n' +
