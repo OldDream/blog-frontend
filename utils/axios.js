29362b751b8d774238eaebbc383e545f419128b1
HYN <huangyuning@lingkehudong.com>
2020-03-03 00:17:10 +0800

    add: 切换线上服务

--- utils/axios.js
+++ utils/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:7001',
+  // baseURL: 'http://127.0.0.1:7001',
+  baseURL: 'http://106.15.67.223:7001/',
   timeout: 6000,
 })
 
