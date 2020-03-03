91266de93e784302a1bf923aee8b8d2769764e06
OldDream <huangyn15@qq.com>
2020-03-03 14:08:41 +0800

    add:配合nginx修改接口

--- utils/axios.js
+++ utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create({
   // baseURL: 'http://127.0.0.1:7001',
-  baseURL: 'http://106.15.67.223:7001/',
+  baseURL: 'http://106.15.67.223/api',
   timeout: 6000,
 })
 
