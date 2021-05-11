acdc7e6515d19dabee0749e46aed1875546fbde4
OldDream <huangyn15@qq.com>
2021-05-11 15:46:35 +0800

    add: 添加 bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,31 +1,40 @@
 // next.config.js
 const withAntdLess = require('next-plugin-antd-less');
-
-module.exports = withAntdLess({
-  // optional
-  // modifyVars: { '@primary-color': '#04f' },
-  // optional
-  // lessVarsFilePath: './src/styles/variables.less',
-  // optional
-  // lessVarsFilePathAppendToEndOfContent: false,
-  // optional https://github.com/webpack-contrib/css-loader#object
-  cssLoaderOptions: {
-    esModule: false,
-    sourceMap: false,
-    modules: {
-      mode: 'local',
-    },
-  },
-
-  // Other Config Here...
-
-  webpack(config) {
-    return config;
-  },
-
-  future: {
-    // if you use webpack5
-    webpack5: true,
-  },
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
 });
+const withPlugins = require('next-compose-plugins');
 
+module.exports = withPlugins([
+  [
+    withAntdLess,
+    {
+      // optional
+      // modifyVars: { '@primary-color': '#04f' },
+      // optional
+      // lessVarsFilePath: './src/styles/variables.less',
+      // optional
+      // lessVarsFilePathAppendToEndOfContent: false,
+      // optional https://github.com/webpack-contrib/css-loader#object
+      cssLoaderOptions: {
+        esModule: false,
+        sourceMap: false,
+        modules: {
+          mode: 'local',
+        },
+      },
+
+      // Other Config Here...
+
+      webpack(config) {
+        return config;
+      },
+
+      future: {
+        // if you use webpack5
+        webpack5: true,
+      },
+    },
+  ],
+  [withBundleAnalyzer, {}],
+]);
