e553dc7ef89e62f44ca09ad6dc3425e231db863f
OldDream <huangyn15@qq.com>
2021-06-09 03:32:23 +0800

    add: 优化导航条样式

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,47 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import style from './style.module.scss';
 import { Row, Col, Menu } from 'antd';
-import { AppstoreOutlined,BugOutlined, BookOutlined,WechatOutlined } from '@ant-design/icons';
+import {
+  AppstoreOutlined,
+  BugOutlined,
+  BookOutlined,
+  WechatOutlined,
+} from '@ant-design/icons';
 
-import Router from 'next/router'
-import axios from '../../utils/axios'
+import Router from 'next/router';
+import axios from '../../utils/axios';
 
 const getIcon = (key) => {
   const hashMap = {
-    'bug': <BugOutlined />,
-    'book': <BookOutlined />,
-    'wechat': <WechatOutlined />,
-  }
-  return hashMap[key]
-}
+    bug: <BugOutlined className={style['addMargin']} />,
+    book: <BookOutlined className={style['addMargin']} />,
+    wechat: <WechatOutlined className={style['addMargin']} />,
+  };
+  return hashMap[key];
+};
 
 const Header = () => {
-  const [navArray, setNavArray] = useState([])
-  const [selectedKey, setSelectedKey] = useState(0)
+  const [navArray, setNavArray] = useState([]);
+  const [selectedKey, setSelectedKey] = useState(0);
   const getNavArray = () => {
     return new Promise((resolve, reject) => {
-      axios.get('/client/articleType').then(res => {
-        resolve(res.data.data)
-      })
-    })
-  }
+      axios.get('/client/articleType').then((res) => {
+        resolve(res.data.data);
+      });
+    });
+  };
 
   useEffect(() => {
-    getNavArray().then(newNavArray => {
-      setNavArray(newNavArray)
-    })
-  }, [])
+    getNavArray().then((newNavArray) => {
+      setNavArray(newNavArray);
+    });
+  }, []);
 
   const handleClick = (e) => {
-    console.log(e)
+    console.log(e);
     if (e.key == 0) {
-      setSelectedKey(e.key)
-      Router.push('/')
+      setSelectedKey(e.key);
+      Router.push('/');
     } else {
-      setSelectedKey(e.key)
-      Router.push(`/articleList?typeId=${e.key}`)
+      setSelectedKey(e.key);
+      Router.push(`/articleList?typeId=${e.key}`);
     }
-  }
+  };
 
   return (
     <div className={style.header}>
@@ -51,26 +56,29 @@ const Header = () => {
           <span className={style.headerText}>慢慢琢磨一些技术。。。</span>
         </Col>
         <Col xs={2} sm={2} md={6} lg={9} xl={8}>
-          <Menu selectedKeys={[selectedKey]} mode="horizontal" className={style.menuStyle} onClick={handleClick}>
+          <Menu
+            selectedKeys={[selectedKey]}
+            mode="horizontal"
+            className={style.menuStyle}
+            onClick={handleClick}
+          >
             <Menu.Item key="0">
-            <AppstoreOutlined />
+              <AppstoreOutlined className={style['addMargin']} />
               全部文章
             </Menu.Item>
-            {
-              navArray.map((item) => {
-                return (
-                  <Menu.Item key={item.id}>
-                    {getIcon(item.icon)}
-                    {item.typeName}
-                  </Menu.Item>
-                )
-              })
-            }
+            {navArray.map((item) => {
+              return (
+                <Menu.Item key={item.id}>
+                  {getIcon(item.icon)}
+                  {item.typeName}
+                </Menu.Item>
+              );
+            })}
           </Menu>
         </Col>
       </Row>
     </div>
   );
-}
+};
 
 export default Header;
